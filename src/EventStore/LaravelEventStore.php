b0b149cc193b08aa6db3d463b7eb8e7231e266e0
Denis Gordo <dengordo@gmail.com>
2018-05-09 10:05:15 +0100

    Laravel 5.6 update and exception fix - use psr log interface instead of Illuminate\Contracts\Logging\Log for laravel 5.6 - fixed wrong reference to EventStreamNotFound

--- src/EventStore/LaravelEventStore.php
+++ src/EventStore/LaravelEventStore.php
@@ -8,7 +8,7 @@ use SmoothPhp\Contracts\Domain\DomainEventStream;
 use SmoothPhp\Contracts\Domain\DomainMessage;
 use SmoothPhp\Contracts\EventStore\DomainEventStreamInterface;
 use SmoothPhp\Contracts\EventStore\EventStore;
-use SmoothPhp\Contracts\EventStore\EventStreamNotFound;
+use SmoothPhp\EventStore\EventStreamNotFound;
 use SmoothPhp\Contracts\Serialization\Serializer;
 use SmoothPhp\Domain\DateTime;
 
